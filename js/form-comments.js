4cebe26b72ab00bc82ffbd6ba0feeaff91072255
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2025-01-04 18:07:18 +0100

    Добавляет модуль с комментариями и правки в CSS

---
+++ js/form-comments.js
@@ -0,0 +1,20 @@
+// const commentForm = document.querySelector('.social__footer');
+// const commentInput = commentForm.querySelector('.social__footer-text');
+// const submitButton = commentForm.querySelector('.social__footer-btn');
+
+// function initCommentForm() {
+//   const pristine = new Pristine(commentForm, {
+//     classTo: 'social__footer-wrapper',
+//     errorTextParent: 'social__footer-wrapper',
+//     errorTextClass: 'social__footer-wrapper--error'
+//   });
+
+//   submitButton .addEventListener('submit', (evt) => {
+//     evt.preventDefault();
+//     if (pristine.validate()) {
+//       // Логика отправки
+//     }
+//   });
+// }
+
+// export { initCommentForm };
